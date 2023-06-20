8c69c71a8f47308feba0c1025f506a56f8b45b64
Harsha <shreeharsha.a@improve10x.com>
2023-06-20 15:12:43 +0530

    Created a model class

---
+++ app/src/main/java/com/example/cryptocurrency/modelclass/Coin.java
@@ -0,0 +1,12 @@
+package com.example.cryptocurrency.modelclass;
+
+public class Coin {
+
+    private String id;
+    private String name;
+    private String symbol;
+    private Integer rank;
+    private Boolean isNew;
+    private Boolean isActive;
+    private String type;
+}
